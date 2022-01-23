072017525f074f8c42225ced06e0cda8ece53f03
onePlusHalf <lkkaushik012@gmail.com>
2022-01-23 14:49:29 +0530

    chore

--- renderer.js
+++ renderer.js
@@ -4,3 +4,62 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+let currentIndex = 0;
+let maxIndex = 12;
+
+function playNext() {
+    if(currentIndex == maxIndex) {
+        const nextButton = document.getElementsByClassName('right')[0];
+
+        nextButton.style.color = 'grey';
+        nextButton.removeAttribute('onclick');
+    } else {
+
+        const previousButton = document.getElementsByClassName('left')[0];
+
+        previousButton.style.color = 'red';
+        previousButton.onclick = function() {
+            console.log('javascript onclick playPrevious');
+            playPrevious();
+        }
+        
+        console.log('Playing next');
+        const caption = document.getElementsByClassName(`subtext-${currentIndex}`)[0];
+
+        const currentVideo = document.getElementById(`video-${currentIndex}`);
+        currentVideo.pause()
+        currentVideo.style.display = 'none';
+        currentIndex += 1;
+        const nextVideo = document.getElementById(`video-${currentIndex}`);
+        nextVideo.style.display = 'block';
+        nextVideo.play();
+    }
+}
+
+function playPrevious() {
+    if(currentIndex == 0) {
+        const previousButton = document.getElementsByClassName('left')[0];
+
+        previousButton.style.color = 'grey';
+        previousButton.removeAttribute('onclick');
+    } else {
+        const nextButton = document.getElementsByClassName('right')[0];
+
+        nextButton.style.color = 'red';
+        nextButton.onclick = function() {
+            console.log('javascript onclick next');
+            playNext();
+        }
+        
+
+        console.log('Playing last');
+        const currentVideo = document.getElementById(`video-${currentIndex}`);
+        currentVideo.pause()
+        currentVideo.style.display = 'none';
+        currentIndex -= 1;
+        const nextVideo = document.getElementById(`video-${currentIndex}`);
+        nextVideo.style.display = 'block';
+        nextVideo.play();
+    }
+}
